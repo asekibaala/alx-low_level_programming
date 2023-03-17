69647d7aff9f711ce33ecb3a48cf58f677becb5b
Abel Sekibaala <abel.sekibaala@gmail.com>
2023-03-17 10:54:35 -0700

    QF

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,14 +15,17 @@ while (e < 58)
 i = 48;
 while (i < 58)
 {
+if (e != i && e < i)
+{
 putchar(e);
 putchar(i);
-if (i == 57 && e == 57)
+if (i == 57 && e == 56)
 {
 break;
 }
 putchar(',');
 putchar(' ');
+}
 i++;
 }
 e++;
