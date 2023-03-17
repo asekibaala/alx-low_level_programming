794caf60aff0c4d159488a747daf0309884cdfaf
Abel Sekibaala <abel.sekibaala@gmail.com>
2023-03-17 09:51:01 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Prints numbers between 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int counter;
+for (counter = 48; counter < 58; counter++)
+{
+putchar(counter);
+}
+putchar('\n');
+return (0);
+}
