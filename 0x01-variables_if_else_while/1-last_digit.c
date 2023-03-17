0725be587dc342f6d348dcfad314f0735f620c40
Abel Sekibaala <abel.sekibaala@gmail.com>
2023-03-17 06:58:21 -0700

    QF

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 }
 else if (ld == 0)
 {
-printf("Last digit of %s is %d and is 0\n", n, ld);
+printf("Last digit of %d is %d and is 0\n", n, ld);
 }
 else
 {
