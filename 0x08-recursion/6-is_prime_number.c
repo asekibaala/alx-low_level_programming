c9942fe4233ef9ba19917db1fab3c0fe23f5cd38
Abel Sekibaala <abel.sekibaala@gmail.com>
2023-04-05 11:20:05 -0700

    fixed bug

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,7 +8,7 @@
  */
 int is_prime(unsigned int n, unsigned int c)
 {
-	if (n % c = 0)
+	if (n % c == 0)
 	{
 		if (n == c)
 			return (1);
@@ -28,7 +28,7 @@ int is_prime_number(int n)
 		return (0);
 	if (n < 0)
 		return (0);
-	if (n = 1)
+	if (n == 1)
 		return (0);
 	return (is_prime(n, 2));
 }
