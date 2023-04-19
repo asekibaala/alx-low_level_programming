bd69d52f6b233f03242dd9a2e341d85872505842
Abel Sekibaala <abel.sekibaala@gmail.com>
2023-04-19 10:15:21 -0700

    bug fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -27,13 +27,13 @@ int main(int argc, char *argv[])
 		exit(2);
 	}
 
-	i = 0;
-	while (i < nbytes)
+	for (i = 0; i < nbytes; i++)
 	{
-	printf("%02x", opc[i] & 0xFF);
+		printf("%02x", opc[i] & 0xFF);
 		if (i != nbytes - 1)
 			printf(" ");
 	}
+
 	printf("\n");
 	return (0);
 }
