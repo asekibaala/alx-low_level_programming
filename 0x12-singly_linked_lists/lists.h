9d50b6187f9ed6469d6200f9b536b0e76deaf3e3
Abel Sekibaala <abel.sekibaala@gmail.com>
2023-04-27 10:11:06 -0700

    quick fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,7 +9,7 @@
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
- * @forward: points to the next node
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for ALX project
@@ -18,7 +18,7 @@ typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_s *forward;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
